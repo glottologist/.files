e520c4671831205012a646542a19b8e7bcf07972
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2022-03-10 17:06:10 +0000

    Added cachix substitutions detail

---
+++ shared/systems/cachix/glottologist.nix
@@ -0,0 +1,10 @@
+{
+  nix.settings = {
+    substituters = [
+      "https://glottologist.cachix.org"
+    ];
+    trusted-public-keys = [
+      "glottologist.cachix.org-1:ixLTihmtGlHsCgDx4umDCcvB+UbMHnb/dsNBbA51WSw="
+    ];
+  };
+}
