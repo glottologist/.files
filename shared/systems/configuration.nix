8c82f8b1561d491d08aff7cad08e8bcd111204e2
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2022-03-11 09:43:59 +0000

    Removed unwanted font packages

--- shared/systems/configuration.nix
+++ shared/systems/configuration.nix
@@ -3,12 +3,10 @@
 let
   basefonts = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
-    cascadia-code
     fira-code
     fira-code-symbols
     fira-mono
     font-awesome
-    nerdfonts
   ];
 in
 {
