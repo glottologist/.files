268535c753c9e522adcca9a9533efa2529cdd70c
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2022-03-14 17:08:54 +0000

    Added hosts file to secrets

--- systems/redtail/networking.nix
+++ systems/redtail/networking.nix
@@ -10,3 +10,31 @@
   };
 
 }
+{ config, pkgs, ... }:
+
+{
+  networking = {
+    hostName = "redtail";
+    useDHCP = false;
+    interfaces = {
+      wlp0s20f3.useDHCP = true;
+    };
+    networkmanager.enable = true;
+    nat = {
+      enable = true;
+      externalInterface = "eth0";
+      internalInterfaces = [ "wg0" ];
+    };
+    firewall = {
+      enable = false;
+      allowedTCPPortRanges = [
+        { from = 1714; to = 1764; }
+      ];
+      allowedTCPPorts = [ 17500 51820 ];
+      allowedUDPPorts = [ 17500 51820 ];
+    };
+
+    extraHosts = builtIns.fromFile ../../secrets/hosts;
+
+  };
+}
