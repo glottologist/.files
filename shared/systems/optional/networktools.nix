3b127b44e777a437ee2b6be78c32a5d65a01f4f3
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2022-03-19 17:15:54 +0000

    Added Telegram desktop and remmina

--- shared/systems/optional/networktools.nix
+++ shared/systems/optional/networktools.nix
@@ -2,14 +2,15 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    dig                     # Domain name server
     curl                    # A command line tool for transferring files with URL syntax
+    dig                     # Domain name server
     networkmanager          # Network configuration and management tool
     networkmanager-openvpn  # NetworkManager's OpenVPN plugin
     openssl                 # Cryptographic library for SSL and TLS protocols
     openvpn                 # A robust and highly flexible tunneling application
+    remmina                 # Remote desktop client written in GTK
     termshark               # A terminal UI for wireshark-cli, inspired by Wireshark
-    wireguard-tools         # Tools for the Wireguard secure network tunnel
     wget                    # Tool for retrieving files using HTTP, HTTPS, and FTP
+    wireguard-tools         # Tools for the Wireguard secure network tunnel
    ];
 }
