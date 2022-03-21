b19debf292fb24e175840c59b8143ba541fe29bc
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2022-03-21 11:10:03 +0000

    Added matter-most desktop to comms role

--- shared/systems/optional/communication.nix
+++ shared/systems/optional/communication.nix
@@ -1,14 +1,15 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    discord         # All-in-one cross-platform voice and text chat for gamers
-    matterbridge    # Simple bridge between Mattermost, IRC, XMPP, Gitter, Slack, Discord, Telegram, Rocket.Chat, Hipchat(via xmpp), Matrix and Steam
-    mattermost      # Open-source Slack-alternative
-    skypeforlinux   # Linux client for Skype
-    slack           # Desktop client for Slack
-    slack-term      # Slack client for your term
-    tdesktop        # Telegram Desktop messaging app
-    thunderbird     # A full-featured e-mail client
-    zoom-us         # zoom.us video conferencing application
+    discord             # All-in-one cross-platform voice and text chat for gamers
+    matterbridge        # Simple bridge between Mattermost, IRC, XMPP, Gitter, Slack, Discord, Telegram, Rocket.Chat, Hipchat(via xmpp), Matrix and Steam
+    mattermost-desktop  # Open-source Slack-alternative
+    mattermost          # Open-source Slack-alternative - desktop version
+    skypeforlinux       # Linux client for Skype
+    slack               # Desktop client for Slack
+    slack-term          # Slack client for your term
+    tdesktop            # Telegram Desktop messaging app
+    thunderbird         # A full-featured e-mail client
+    zoom-us             # zoom.us video conferencing application
    ];
 }
