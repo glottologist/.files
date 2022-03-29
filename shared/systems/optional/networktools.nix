5bd0ddbe2a3b8c1c1ce666b2db21f5e5113cfe26
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2022-03-29 12:16:51 +0100

    Added ookla speedtest

--- shared/systems/optional/networktools.nix
+++ shared/systems/optional/networktools.nix
@@ -5,13 +5,14 @@ let
     dig                     # Domain name server
     networkmanager          # Network configuration and management tool
     networkmanager-openvpn  # NetworkManager's OpenVPN plugin
+    nmap-graphical          # Graphical version of Nmap
+    ookla-speedtest         # Command line internet speedtest tool by Ookla
     openssl                 # Cryptographic library for SSL and TLS protocols
     openvpn                 # A robust and highly flexible tunneling application
     remmina                 # Remote desktop client written in GTK
     termshark               # A terminal UI for wireshark-cli, inspired by Wireshark
     wget                    # Tool for retrieving files using HTTP, HTTPS, and FTP
     wireguard-tools         # Tools for the Wireguard secure network tunnel
-    nmap-graphical          # Graphical version of Nmap
    ];
  in
 {
