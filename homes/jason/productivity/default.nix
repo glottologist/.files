ccfa204b1efe990a8e899e05790b8870d6890a80
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2022-05-31 09:22:42 +0100

    Added inkscape to home

--- homes/jason/productivity/default.nix
+++ homes/jason/productivity/default.nix
@@ -5,13 +5,14 @@
   ];
 
   home.packages = with pkgs; [
-    dstask            # Command line todo list with super-reliable git sync
-    rainbowstream     # Streaming command-line twitter client
-    adobe-reader      # PDF Reader
-    gimp              # GNU image editor
-    krita             # A free and open source painting application
-    digikam           # Photo Management Program
-    pixeluvo          # A Beautifully Designed Image and Photo Editor for Windows and Linux
+    dstask                      # Command line todo list with super-reliable git sync
+    rainbowstream               # Streaming command-line twitter client
+    adobe-reader                # PDF Reader
+    gimp                        # GNU image editor
+    krita                       # A free and open source painting application
+    digikam                     # Photo Management Program
+    pixeluvo                    # A Beautifully Designed Image and Photo Editor for Windows and Linux
+    inkscape-with-extensions    # Vector graphics editor
   ];
 
   programs = {
