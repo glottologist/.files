73084e85a4bfd07fd9070af58c5d586635ab6396
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2022-07-13 09:01:09 +0100

    Added postman

--- shared/home/development/default.nix
+++ shared/home/development/default.nix
@@ -11,12 +11,14 @@
   home.packages = with pkgs; [
     remarshal         # Convert between TOML, YAML and JSON
     pkg-config        # A tool that allows packages to find out information about other packages (wrapper script)
-    ctags             # A tool for fast source code browsing (exuberant ctags)
-    #universal-ctags  # A maintained ctags implementation
+    #ctags             # A tool for fast source code browsing (exuberant ctags)
+    universal-ctags   # A maintained ctags implementation
     silver-searcher   # Ack like searcher focused on code
     difftastic        # A syntax-aware diff
     rpi-imager        # Raspberry Pi Imaging Utility
     openvscode-server # Open source VSCode Remote Server
+    postman           # API Development Environment
+    newman            # A command line runner for Postman
   ];
 
 
