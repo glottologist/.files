4e83c9c9ad6f70ef62d55140e7c8376806ce9429
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2022-08-01 12:56:38 +0100

    Added Proton mail bridge and hydroxide

--- shared/home/productivity/default.nix
+++ shared/home/productivity/default.nix
@@ -13,6 +13,9 @@
     digikam                     # Photo Management Program
     pixeluvo                    # A Beautifully Designed Image and Photo Editor for Windows and Linux
     inkscape-with-extensions    # Vector graphics editor
+    protonmail-bridge           # Use your ProtonMail account with your local e-mail client
+    hydroxide                   # A third-party, open-source ProtonMail bridge
+    electron-mail               # ElectronMail is an Electron-based unofficial desktop client for ProtonMail
   ];
 
   programs = {
