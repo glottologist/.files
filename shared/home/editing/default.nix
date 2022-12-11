41c9aae5a597a4bd8abadcfb91f4e1a2a4c7759a
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2022-12-11 04:56:24 +0000

    Added editing

---
+++ shared/home/editing/default.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, stdenv, ... }:
+{
+
+  imports = [
+  ];
+
+  home.packages = with pkgs; [
+    davinci-resolve        # Professional Video Editing, Color, Effects and Audio Post
+  ];
+
+  programs = {
+  };
+
+  services = {
+  };
+}
