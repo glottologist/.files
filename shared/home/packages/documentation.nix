e46765b640b4530485b99b045232fec530dcab16
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-02-21 14:22:29 +0000

    Adeed html to pdf tool

--- shared/home/packages/documentation.nix
+++ shared/home/packages/documentation.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    freeoffice                # An office suite with a word processor, spreadsheet and presentation program
-    libreoffice               # Comprehensive, professional-quality productivity suite, a variant of openoffice.org
     briss                     # Java application for cropping PDF files
     diff-pdf                  # Simple tool for visually comparing two PDF files
+    freeoffice                # An office suite with a word processor, spreadsheet and presentation program
     img2pdf                   # Convert images to PDF via direct JPEG inclusion
     latex2html                # LaTeX-to-HTML translator
+    libreoffice               # Comprehensive, professional-quality productivity suite, a variant of openoffice.org
     mdbook                    # Create books from MarkDown
     notepadqq                 # Notepad++-like editor for the Linux desktop
     obsidian                  # A powerful knowledge base that works on top of a local folder of plain text Markdown files
@@ -16,5 +16,6 @@
     plantuml-server           # A web application to generate UML diagrams on-the-fly.
     texmaker                  # TeX and LaTeX editor
     texstudio                 # TeX and LaTeX editor
+    wkhtmltopdf               # Tools for rendering web pages to PDF or images (binary package)
   ];
 }
