026b2615cd0fabd2faa28c66d55625b09c165bae
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-04-25 10:29:03 +0100

    Added additional browsers

--- shared/systems/optional/browsers.nix
+++ shared/systems/optional/browsers.nix
@@ -5,6 +5,7 @@
      firefox                # A web browser built from Firefox source tree
      qutebrowser            # Keyboard-focused browser with a minimal GUI
      tor-browser-bundle-bin # Tor Browser Bundle built by torproject.org
+     nyxt                   # Infinitely extensible web-browser (with Lisp development files using WebKitGTK platform port)
 
    ];
 }
