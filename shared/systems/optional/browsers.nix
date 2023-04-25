c5dc27ded054e97f8711be6f7c78ba73c8a132ba
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-04-25 10:15:15 +0100

    Added additional browsers

--- shared/systems/optional/browsers.nix
+++ shared/systems/optional/browsers.nix
@@ -1,7 +1,10 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-     brave       # Privacy-oriented browser for Desktop and Laptop computers
-     firefox     # A web browser built from Firefox source tree
+     brave                  # Privacy-oriented browser for Desktop and Laptop computers
+     firefox                # A web browser built from Firefox source tree
+     qutebrowser            # Keyboard-focused browser with a minimal GUI
+     tor-browser-bundle-bin # Tor Browser Bundle built by torproject.org
+
    ];
 }
