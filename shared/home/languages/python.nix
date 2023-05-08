acc1970c2a88ac4519cda1d36342eb459fca52e4
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-05-08 14:54:42 +0100

    Updated Python

--- shared/home/languages/python.nix
+++ shared/home/languages/python.nix
@@ -1,16 +1,16 @@
 { pkgs, ...}:
 {
   home.packages = with pkgs; [
-     python39
-     python39Packages.pandas
-     python39Packages.pip
-     python39Packages.setuptools
-     python39Packages.pdf2image
-     python39Packages.tomlkit
-     python39Packages.pynvim
-     #python39Packages.pyparsing
-     python39Packages.pylru
      nodePackages.coc-python                 # python extension for coc
+     python310Full
+     python310Packages.pandas
+     python310Packages.pdf2image
+     python310Packages.pelican
+     python310Packages.pip
+     python310Packages.pylru
+     python310Packages.pynvim
+     python310Packages.setuptools
+     python310Packages.tomlkit
   ];
 
 }
