095fa73ec5f8a4dfffa4c02aaa8a92316fbc3d6d
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-08-08 16:41:32 +0300

    Changed Haskelig font to not strip root

--- shared/home/fonts/default.nix
+++ shared/home/fonts/default.nix
@@ -7,7 +7,7 @@
   build-font = pname: phash: pzip: striproot: pkgs.callPackage ./fonts/build-font {inherit nfversion pname phash pzip striproot;};
   jetbrainsmono-nerdfont = build-font "jetbrainsmono-nerdfont" "p6i6CTlDCGXH+puCINM69n4fLoIwBTgskbSBi7EbkJc=" "JetBrainsMono.zip" true;
   firacode-nerdfont = build-font "firacode-nerdfont" "AeXvD9xhaPscfIUb7nvubzK664Bk1f1lNygAYhE95nk=" "Firacode.zip" false;
-  hasklig-nerdfont = build-font "hasklig-nerdfont" "Shtvt79mzVdrXytWNdkTUQXr5VnSbUPXAwsOp9ZqX3c=" "Hasklig.zip" true;
+  hasklig-nerdfont = build-font "hasklig-nerdfont" "Shtvt79mzVdrXytWNdkTUQXr5VnSbUPXAwsOp9ZqX3c=" "Hasklig.zip" false;
 in {
   home.packages = with pkgs; [
     fira-code
