113a60c3cb60c3313ad33588b6db4c44ba345969
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-08-08 14:31:17 +0300

    Removed davinci

--- shared/home/editing/default.nix
+++ shared/home/editing/default.nix
@@ -5,7 +5,7 @@
   ];
 
   home.packages = with pkgs; [
-    davinci-resolve        # Professional Video Editing, Color, Effects and Audio Post
+    # davinci-resolve        # Professional Video Editing, Color, Effects and Audio Post
   ];
 
   programs = {
