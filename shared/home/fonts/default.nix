9b606ca5e1459f266351237735415452e62fb750
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-08-08 16:43:50 +0300

    Changed Haskelig font hash

--- shared/home/fonts/default.nix
+++ shared/home/fonts/default.nix
@@ -7,7 +7,7 @@
   build-font = pname: phash: pzip: striproot: pkgs.callPackage ./fonts/build-font {inherit nfversion pname phash pzip striproot;};
   jetbrainsmono-nerdfont = build-font "jetbrainsmono-nerdfont" "p6i6CTlDCGXH+puCINM69n4fLoIwBTgskbSBi7EbkJc=" "JetBrainsMono.zip" true;
   firacode-nerdfont = build-font "firacode-nerdfont" "AeXvD9xhaPscfIUb7nvubzK664Bk1f1lNygAYhE95nk=" "Firacode.zip" false;
-  hasklig-nerdfont = build-font "hasklig-nerdfont" "Shtvt79mzVdrXytWNdkTUQXr5VnSbUPXAwsOp9ZqX3c=" "Hasklig.zip" false;
+  hasklig-nerdfont = build-font "hasklig-nerdfont" "pfZrr7jB8gP/4wW+6qR5WZK2cnLPn6+lZGAH1gRG4Rw=" "Hasklig.zip" false;
 in {
   home.packages = with pkgs; [
     fira-code
