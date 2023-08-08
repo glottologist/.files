4974ebc02abb8e119c050a0bb56b510a0d55e41b
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-08-08 16:37:52 +0300

    Changed firacode font hash

--- shared/home/fonts/default.nix
+++ shared/home/fonts/default.nix
@@ -1,12 +1,14 @@
-{ config, pkgs, ... }:
-let
-  nfversion = "3.0.2";
-  build-font = pname: phash: pzip: striproot:  pkgs.callPackage ./fonts/build-font { inherit nfversion pname phash pzip striproot; };
-  jetbrainsmono-nerdfont = build-font "jetbrainsmono-nerdfont" "p6i6CTlDCGXH+puCINM69n4fLoIwBTgskbSBi7EbkJc=" "JetBrainsMono.zip" true;
-  firacode-nerdfont = build-font "firacode-nerdfont" "AKjJ/KN+hBpoIXCo3KlRk1EHOZN2Bqc1g036zLdXLrs=" "Firacode.zip" false;
-  hasklig-nerdfont = build-font "hasklig-nerdfont" "Shtvt79mzVdrXytWNdkTUQXr5VnSbUPXAwsOp9ZqX3c=" "Hasklig.zip" true ;
-in
 {
+  config,
+  pkgs,
+  ...
+}: let
+  nfversion = "3.0.2";
+  build-font = pname: phash: pzip: striproot: pkgs.callPackage ./fonts/build-font {inherit nfversion pname phash pzip striproot;};
+  jetbrainsmono-nerdfont = build-font "jetbrainsmono-nerdfont" "p6i6CTlDCGXH+puCINM69n4fLoIwBTgskbSBi7EbkJc=" "JetBrainsMono.zip" true;
+  firacode-nerdfont = build-font "firacode-nerdfont" "AeXvD9xhaPscfIUb7nvubzK664Bk1f1lNygAYhE95nk=" "Firacode.zip" false;
+  hasklig-nerdfont = build-font "hasklig-nerdfont" "Shtvt79mzVdrXytWNdkTUQXr5VnSbUPXAwsOp9ZqX3c=" "Hasklig.zip" true;
+in {
   home.packages = with pkgs; [
     fira-code
     fira-code-symbols
