ffe8a88c3f74250130c8123b14456a04fdf70f15
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-08-08 16:51:46 +0300

    Changed firacode font hash

--- shared/home/fonts/default.nix
+++ shared/home/fonts/default.nix
@@ -5,7 +5,7 @@
 }: let
   nfversion = "3.0.2";
   build-font = pname: phash: pzip: striproot: pkgs.callPackage ./fonts/build-font {inherit nfversion pname phash pzip striproot;};
-  jetbrainsmono-nerdfont = build-font "jetbrainsmono-nerdfont" "p6i6CTlDCGXH+puCINM69n4fLoIwBTgskbSBi7EbkJc=" "JetBrainsMono.zip" false;
+  jetbrainsmono-nerdfont = build-font "jetbrainsmono-nerdfont" "JIodr+3KuVvWMVmvTd0KgYMwsLfHgk/5Hvx/z/DOkZk=" "JetBrainsMono.zip" false;
   firacode-nerdfont = build-font "firacode-nerdfont" "AeXvD9xhaPscfIUb7nvubzK664Bk1f1lNygAYhE95nk=" "Firacode.zip" false;
   hasklig-nerdfont = build-font "hasklig-nerdfont" "pfZrr7jB8gP/4wW+6qR5WZK2cnLPn6+lZGAH1gRG4Rw=" "Hasklig.zip" false;
 in {
