b56c425368ba0972e584037aa7829dd4a7655899
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-08-09 11:13:48 +0300

    Added Jupyter

--- shared/home/development/default.nix
+++ shared/home/development/default.nix
@@ -1,6 +1,10 @@
-{ config, lib, pkgs, stdenv, ... }:
 {
-
+  config,
+  lib,
+  pkgs,
+  stdenv,
+  ...
+}: {
   imports = [
     ./git/default.nix
     ./neovim/default.nix
@@ -9,26 +13,25 @@
   ];
 
   home.packages = with pkgs; [
-    difftastic        # A syntax-aware diff
-    earthly           # Build automation for the container era
-    helix             # A post-modern modal text editor
-    netlify-cli       # CLI to manage netlify deployments
-    newman            # A command line runner for Postman
-    pkg-config        # A tool that allows packages to find out information about other packages (wrapper script)
-    postman           # API Development Environment
-    remarshal         # Convert between TOML, YAML and JSON
-    rpi-imager        # Raspberry Pi Imaging Utility
-    silver-searcher   # Ack like searcher focused on code
-    universal-ctags   # A maintained ctags implementation
-    usbutils          # USb Utlities
+    difftastic # A syntax-aware diff
+    earthly # Build automation for the container era
+    helix # A post-modern modal text editor
+    jupyter # The Jupyter HTML notebook is a web-based notebook environment for interactive computing
+    netlify-cli # CLI to manage netlify deployments
+    newman # A command line runner for Postman
+    pkg-config # A tool that allows packages to find out information about other packages (wrapper script)
+    postman # API Development Environment
+    remarshal # Convert between TOML, YAML and JSON
+    rpi-imager # Raspberry Pi Imaging Utility
+    silver-searcher # Ack like searcher focused on code
+    universal-ctags # A maintained ctags implementation
+    usbutils # USb Utlities
   ];
-
 
   programs = {
   };
 
   services = {
     #lorri.enable = true;
-
   };
 }
