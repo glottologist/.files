8a2a35be968c6a05b42bb0a68197ed02b9c75d66
Jason Ridgway-Taylor <jason@ridgway-taylor.co.uk>
2025-05-22 13:57:00 +0300

    refactor(fish): rename and improve jj bookmark tracking function

--- shared/terminal/fish/functions/jj_track_bookmark.fish
+++ shared/terminal/fish/functions/jj_track_bookmark.fish
@@ -1,8 +1,9 @@
-function track-jj-bookmark --argument-names branch_name --description "Track a git branch with jj bookmark"
-    if test -z "$branch_name"
-        echo "Error: Please provide a branch name"
+function jj_track_bookmark
+    if test (count $argv) -lt 1
+        echo "Usage: track_jj_bookmark <branch_name>"
         return 1
     end
+    set branch_name $argv[1]
     
     jj bookmark track "$branch_name@origin"
 end
