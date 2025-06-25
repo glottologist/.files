3275188960783d285ab7565d726aebb3aa1ed94b
jason <jason@ridgway-taylor.co.uk>
2025-06-25 12:24:53 +0100

    feat: added larger local models

--- hosts/common/ai.nix
+++ hosts/common/ai.nix
@@ -1,26 +1,30 @@
- 
-
 {
   config,
   pkgs,
   ...
 }: {
-
-   services.ollama = {
+  services.ollama = {
     enable = true;
-    loadModels = [ "llama3.2:3b"  ];
+    loadModels = ["llama4:latest" "deepseek-r1:14b" "gemma3:27b"];
     acceleration = "rocm";
     host = "127.0.0.1";
     port = 11434;
   };
-
-
+  systemd.services.ollama.serviceConfig = {
+    Environment = ["OLLAMA_HOST=0.0.0.0:11434"];
+  };
 
   services.open-webui = {
     enable = true;
     port = 8888;
     host = "127.0.0.1";
+    package = pkgs.open-webui; # pkgs must be from stable, for example nixos-24.11
+    environment = {
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
+      OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+    };
   };
-
 }
-
--- shared/terminal/foot/default.nix
+++ shared/terminal/foot/default.nix
@@ -51,6 +51,6 @@
     };
   };
 
-  # xdg.configFile."foot/foot-theme.ini".source = ./theme.ini;
+  xdg.configFile."foot/foot-theme.ini".source = ./theme.ini;
   # xdg.configFile."foot/foot.ini".source = ./theme.ini;
 }
