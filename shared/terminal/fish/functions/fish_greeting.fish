bf634204e23d8e0e9e6565aa72dfa014ce8ce698
jason <jason@ridgway-taylor.co.uk>
2025-07-10 21:04:12 +0100

    feat: added neofetch for fish greeting

--- shared/terminal/fish/functions/fish_greeting.fish
+++ shared/terminal/fish/functions/fish_greeting.fish
@@ -1,6 +1,5 @@
 function fish_greeting
     neofetch
     set_color white; echo " Langs:  Rust | Ocaml | Go | Haskell | Scala | Idris | Python | Typescript (if I have to)"
-    set_color green; echo " Shell:  󰈺 Fish"
     # clear
 end
