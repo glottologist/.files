364e9133c624e53a4f09fdccb8e00c2ff3809aca
jason <jason@ridgway-taylor.co.uk>
2025-07-16 12:46:40 +0100

    fix: removed neovim plugin from vscode

--- shared/development/vscode/default.nix
+++ shared/development/vscode/default.nix
@@ -1,63 +1,66 @@
-{ config, lib, pkgs, stdenv, ... }:
-let
+{
+  config,
+  lib,
+  pkgs,
+  stdenv,
+  ...
+}: let
   inherit (pkgs) vscode-utils vscode-extensions;
-  native-ext = with vscode-extensions;
-      [
-        arcticicestudio.nord-visual-studio-code
-        asvetliakov.vscode-neovim
-        b4dm4n.vscode-nixpkgs-fmt
-        bbenoist.nix
-        brandonkirbyson.solarized-palenight
-        catppuccin.catppuccin-vsc-icons
-        esbenp.prettier-vscode
-        formulahendry.code-runner
-        foxundermoon.shell-format
-        fstarlang.fstar-vscode-assistant
-        github.vscode-github-actions
-        github.vscode-pull-request-github
-        gitlab.gitlab-workflow
-        golang.go
-        gruntfuggly.todo-tree
-        haskell.haskell
-        ibm.output-colorizer
-        ionide.ionide-fsharp
-        justusadam.language-haskell
-        kamikillerto.vscode-colorize
-        maximedenes.vscoq
-        mkhl.direnv
-        ms-python.black-formatter
-        ms-python.python
-        ms-toolsai.jupyter
-        ms-vscode-remote.remote-containers
-        ms-vscode-remote.remote-ssh
-        ms-vsliveshare.vsliveshare
-        ocamllabs.ocaml-platform
-        pkief.material-icon-theme
-        redhat.vscode-yaml
-        rust-lang.rust-analyzer
-        scala-lang.scala
-        scalameta.metals
-        vue.vscode-typescript-vue-plugin
-        zhuangtongfa.material-theme
-
-      ];
+  native-ext = with vscode-extensions; [
+    arcticicestudio.nord-visual-studio-code
+    # asvetliakov.vscode-neovim
+    b4dm4n.vscode-nixpkgs-fmt
+    bbenoist.nix
+    brandonkirbyson.solarized-palenight
+    catppuccin.catppuccin-vsc-icons
+    esbenp.prettier-vscode
+    formulahendry.code-runner
+    foxundermoon.shell-format
+    fstarlang.fstar-vscode-assistant
+    github.vscode-github-actions
+    github.vscode-pull-request-github
+    gitlab.gitlab-workflow
+    golang.go
+    gruntfuggly.todo-tree
+    haskell.haskell
+    ibm.output-colorizer
+    ionide.ionide-fsharp
+    justusadam.language-haskell
+    kamikillerto.vscode-colorize
+    maximedenes.vscoq
+    mkhl.direnv
+    ms-python.black-formatter
+    ms-python.python
+    ms-toolsai.jupyter
+    ms-vscode-remote.remote-containers
+    ms-vscode-remote.remote-ssh
+    ms-vsliveshare.vsliveshare
+    ocamllabs.ocaml-platform
+    pkief.material-icon-theme
+    redhat.vscode-yaml
+    rust-lang.rust-analyzer
+    scala-lang.scala
+    scalameta.metals
+    vue.vscode-typescript-vue-plugin
+    zhuangtongfa.material-theme
+  ];
 
   built-ext = pkgs.vscode-utils.extensionsFromVscodeMarketplace (import ./extensions.nix).extensions;
 
-  ext = (native-ext) ++ built-ext;
+  ext = native-ext ++ built-ext;
 
   settings = (import ./settings.nix).user;
 
   code = pkgs.vscode;
 
-  overridePackage = (pkgs.vscode-with-extensions.override {
-    vscode = code;
-    vscodeExtensions = ext;
-  }).overrideAttrs (old: {
-    inherit (code) pname version;
-  });
-in
-{
+  overridePackage =
+    (pkgs.vscode-with-extensions.override {
+      vscode = code;
+      vscodeExtensions = ext;
+    }).overrideAttrs (old: {
+      inherit (code) pname version;
+    });
+in {
   programs.vscode = {
     enable = true;
     package = overridePackage;
@@ -69,5 +72,4 @@ in
   ];
 
   services.vscode-server.enable = true;
-
 }
