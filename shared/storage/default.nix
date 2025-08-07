3cecf3ec00bcd2073d93b7de9823fd2db887060e
jason <jason@ridgway-taylor.co.uk>
2025-08-07 17:36:15 +0100

    chore: removed maestral

--- shared/storage/default.nix
+++ shared/storage/default.nix
@@ -5,31 +5,9 @@
   stdenv,
   ...
 }: {
-  # systemd.user.services.maestral = {
-  #   Unit = {
-  #     Description = "Maestral daemon";
-  #     After = ["graphical-session-pre.target"];
-  #     PartOf = ["graphical-session.target"];
-  #   };
-  #   Install = {
-  #     WantedBy = ["graphical-session.target"];
-  #   };
-  #   Service = {
-  #     ExecStart = "${pkgs.maestral}/bin/maestral start -f";
-  #     ExecStop = "${pkgs.maestral}/bin/maestral stop";
-  #     Restart = "on-failure";
-  #     Nice = 10;
-  #   };
-  # };
   home.packages = with pkgs; [
-    maestral-gui
-    maestral
     xivlauncher
   ];
-  # services.dropbox = {
-  #   enable = true;
-  #   path = "${config.home.homeDirectory}/Dropbox"; # Optional custom path
-  # };
   imports = [
   ];
 }
