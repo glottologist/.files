200802dbbdb3b96fd14e230bae215455bb1181a6
jason <jason@ridgway-taylor.co.uk>
2025-08-08 17:28:33 +0100

    feat: added app indicator support

--- shared/wm/waybar/waybar-glottologist2.nix
+++ shared/wm/waybar/waybar-glottologist2.nix
@@ -23,6 +23,10 @@
   base0F = "dc8a78"; # Rosewater - deprecated, tags
 in
   with lib; {
+    home.packages = with pkgs; [
+      libappindicator-gtk3
+      libappindicator
+    ];
     # Configure & Theme Waybar
     programs.waybar = {
       enable = true;
@@ -49,7 +53,7 @@ in
             on-scroll-down = "hyprctl dispatch workspace e-1";
           };
           "clock" = {
-            format = '' {:%H:%M}'';
+            format = ''{:%H:%M}'';
             /*
             ''{: %I:%M %p}'';
             */
@@ -85,8 +89,8 @@ in
             tooltip = false;
             on-click = "${terminal} -e speedtest";
           };
-		  
-		   "custom/systemstats" = {
+
+          "custom/systemstats" = {
             format = "{}";
             interval = 5;
             exec = "echo \"󰍛 $(free | awk '/Mem:/ {printf \"%.0f%%\", ($3/$2)*100}') | 󰻠 $(cat /proc/loadavg | awk '{printf \"%.1f\", $1}') | 󰋊 $(df -h / | awk 'NR==2 {print $5}')\"";
@@ -168,184 +172,184 @@ in
       ];
       style = concatStrings [
         ''
-          * {
-            font-size: 14px;
-            font-family: JetBrainsMono Nerd Font, Font Awesome, sans-serif;
-            font-weight: bold;
-          }
-          window#waybar {
-            /*
+                * {
+                  font-size: 14px;
+                  font-family: JetBrainsMono Nerd Font, Font Awesome, sans-serif;
+                  font-weight: bold;
+                }
+                window#waybar {
+                  /*
 
-              background-color: rgba(26,27,38,0);
-              border-bottom: 1px solid rgba(26,27,38,0);
-              border-radius: 0px;
-              color: #${base0F};
-            */
+                    background-color: rgba(26,27,38,0);
+                    border-bottom: 1px solid rgba(26,27,38,0);
+                    border-radius: 0px;
+                    color: #${base0F};
+                  */
 
-            background-color: rgba(26,27,38,0);
-            border-bottom: 1px solid rgba(26,27,38,0);
-            border-radius: 0px;
-            color: #${base0F};
-          }
-          #workspaces {
-            /*
-              Eternal
-              background: linear-gradient(180deg, #${base00}, #${base01});
-              margin: 5px 5px 5px 0px;
-              padding: 0px 10px;
-              border-radius: 0px 15px 15px 0px;
-              border: 0px;
-              font-style: normal;
-              color: #${base00};
-            */
-            background: linear-gradient(45deg, #${base01}, #${base01});
-            margin: 2px;
-            padding: 0px 1px;
-            border-radius: 15px;
-            border: 0px;
-            font-style: normal;
-            color: #${base00};
-          }
-          #workspaces button {
-            padding: 0px 5px;
-            margin: 4px 3px;
-            border-radius: 15px;
-            border: 0px;
-            color: #${base00};
-            background: linear-gradient(45deg, #${base0D}, #${base0E});
-            opacity: 0.5;
-            transition: all 0.3s ease-in-out;
-          }
-          #workspaces button.active {
-            padding: 0px 5px;
-            margin: 4px 3px;
-            border-radius: 15px;
-            border: 0px;
-            color: #${base00};
-            background: linear-gradient(45deg, #${base0D}, #${base0E});
-            opacity: 1.0;
-            min-width: 40px;
-            transition: all 0.3s ease-in-out;
-          }
-          #workspaces button:hover {
-            border-radius: 15px;
-            color: #${base00};
-            background: linear-gradient(45deg, #${base0D}, #${base0E});
-            opacity: 0.8;
-          }
-          tooltip {
-            background: #${base00};
-            border: 1px solid #${base0E};
-            border-radius: 10px;
-          }
-          tooltip label {
-            color: #${base07};
-          }
-          #window {
-            margin: 2px;
-            padding: 2px 20px;
-            color: #${base05};
-            background: #${base01};
-            border-radius: 15px 15px 15px 15px;
-          }
-          #memory {
-            color: #${base0F};
-            background: #${base01};
-            margin: 2px;
-            padding: 2px 20px;
-            border-radius: 15px 15px 15px 15px;
-          }
-          #clock {
-            color: #${base0B};
-              background: #${base00};
-              border-radius: 15px 15px 15px 15px;
-              margin: 2px;
-              padding: 2px 20px;
-          }
-          #idle_inhibitor {
-            color: #${base0A};
-              background: #${base00};
-              border-radius: 15px 15px 15px 15px;
-              margin: 2px;
-              padding: 2px 20px;
-          }
-          #cpu {
-            color: #${base07};
-              background: #${base00};
-              border-radius: 15px 15px 15px 15px;
-              margin: 2px;
-              padding: 2px 20px;
-          }
-		  #custom-systemstats {
-            color: #${base0C};
-              background: #${base00};
-              border-radius: 15px 15px 15px 15px;
-              margin: 2px;
-              padding: 2px 20px;
-          }
-          #disk {
-            color: #${base0F};
-              background: #${base00};
-              border-radius: 15px 15px 15px 15px;
-              margin: 2px;
-              padding: 2px 20px;
-          }
-          #battery {
-            color: #${base08};
-            background: #${base00};
-            border-radius: 15px 15px 15px 15px;
-            margin: 2px;
-            padding: 2px 20px;
-          }
-          #network {
-            color: #${base09};
-            background: #${base00};
-            border-radius: 15px 15px 15px 15px;
-            margin: 2px;
-            padding: 2px 20px;
-          }
-          #tray {
-            color: #${base05};
-            background: #${base00};
-            border-radius: 15px 15px 15px 15px;
-            margin: 2px;
-            padding: 2px 15px;
-          }
-          #pulseaudio {
-            color: #${base0D};
-            background: #${base01};
-            margin: 2px;
-            padding: 2px 20px;
-            border-radius: 15px 15px 15px 15px;
-          }
-          #custom-notification {
-            color: #${base0C};
-            background: #${base00};
-            border-radius: 15px 15px 15px 15px;
-            margin: 2px;
-            padding: 2px 20px;
-          }
-          #custom-startmenu {
-            color: #${base0E};
-            background: #${base00};
-            border-radius: 0px 15px 15px 0px;
-            margin: 2px 2px 2px 0px;
-            padding: 2px 20px;
-          }
-          #idle_inhibitor {
-            color: #${base09};
-            background: #${base00};
-            border-radius: 15px 15px 15px 15px;
-            margin: 2px;
-            padding: 2px 20px;
-          }
-          #custom-exit {
-            color: #${base0E};
-            background: #${base00};
-            border-radius: 15px 0px 0px 15px;
-            margin: 2px 0px 2px 2px;
-            padding: 2px 20px;
-          }
+                  background-color: rgba(26,27,38,0);
+                  border-bottom: 1px solid rgba(26,27,38,0);
+                  border-radius: 0px;
+                  color: #${base0F};
+                }
+                #workspaces {
+                  /*
+                    Eternal
+                    background: linear-gradient(180deg, #${base00}, #${base01});
+                    margin: 5px 5px 5px 0px;
+                    padding: 0px 10px;
+                    border-radius: 0px 15px 15px 0px;
+                    border: 0px;
+                    font-style: normal;
+                    color: #${base00};
+                  */
+                  background: linear-gradient(45deg, #${base01}, #${base01});
+                  margin: 2px;
+                  padding: 0px 1px;
+                  border-radius: 15px;
+                  border: 0px;
+                  font-style: normal;
+                  color: #${base00};
+                }
+                #workspaces button {
+                  padding: 0px 5px;
+                  margin: 4px 3px;
+                  border-radius: 15px;
+                  border: 0px;
+                  color: #${base00};
+                  background: linear-gradient(45deg, #${base0D}, #${base0E});
+                  opacity: 0.5;
+                  transition: all 0.3s ease-in-out;
+                }
+                #workspaces button.active {
+                  padding: 0px 5px;
+                  margin: 4px 3px;
+                  border-radius: 15px;
+                  border: 0px;
+                  color: #${base00};
+                  background: linear-gradient(45deg, #${base0D}, #${base0E});
+                  opacity: 1.0;
+                  min-width: 40px;
+                  transition: all 0.3s ease-in-out;
+                }
+                #workspaces button:hover {
+                  border-radius: 15px;
+                  color: #${base00};
+                  background: linear-gradient(45deg, #${base0D}, #${base0E});
+                  opacity: 0.8;
+                }
+                tooltip {
+                  background: #${base00};
+                  border: 1px solid #${base0E};
+                  border-radius: 10px;
+                }
+                tooltip label {
+                  color: #${base07};
+                }
+                #window {
+                  margin: 2px;
+                  padding: 2px 20px;
+                  color: #${base05};
+                  background: #${base01};
+                  border-radius: 15px 15px 15px 15px;
+                }
+                #memory {
+                  color: #${base0F};
+                  background: #${base01};
+                  margin: 2px;
+                  padding: 2px 20px;
+                  border-radius: 15px 15px 15px 15px;
+                }
+                #clock {
+                  color: #${base0B};
+                    background: #${base00};
+                    border-radius: 15px 15px 15px 15px;
+                    margin: 2px;
+                    padding: 2px 20px;
+                }
+                #idle_inhibitor {
+                  color: #${base0A};
+                    background: #${base00};
+                    border-radius: 15px 15px 15px 15px;
+                    margin: 2px;
+                    padding: 2px 20px;
+                }
+                #cpu {
+                  color: #${base07};
+                    background: #${base00};
+                    border-radius: 15px 15px 15px 15px;
+                    margin: 2px;
+                    padding: 2px 20px;
+                }
+          #custom-systemstats {
+                  color: #${base0C};
+                    background: #${base00};
+                    border-radius: 15px 15px 15px 15px;
+                    margin: 2px;
+                    padding: 2px 20px;
+                }
+                #disk {
+                  color: #${base0F};
+                    background: #${base00};
+                    border-radius: 15px 15px 15px 15px;
+                    margin: 2px;
+                    padding: 2px 20px;
+                }
+                #battery {
+                  color: #${base08};
+                  background: #${base00};
+                  border-radius: 15px 15px 15px 15px;
+                  margin: 2px;
+                  padding: 2px 20px;
+                }
+                #network {
+                  color: #${base09};
+                  background: #${base00};
+                  border-radius: 15px 15px 15px 15px;
+                  margin: 2px;
+                  padding: 2px 20px;
+                }
+                #tray {
+                  color: #${base05};
+                  background: #${base00};
+                  border-radius: 15px 15px 15px 15px;
+                  margin: 2px;
+                  padding: 2px 15px;
+                }
+                #pulseaudio {
+                  color: #${base0D};
+                  background: #${base01};
+                  margin: 2px;
+                  padding: 2px 20px;
+                  border-radius: 15px 15px 15px 15px;
+                }
+                #custom-notification {
+                  color: #${base0C};
+                  background: #${base00};
+                  border-radius: 15px 15px 15px 15px;
+                  margin: 2px;
+                  padding: 2px 20px;
+                }
+                #custom-startmenu {
+                  color: #${base0E};
+                  background: #${base00};
+                  border-radius: 0px 15px 15px 0px;
+                  margin: 2px 2px 2px 0px;
+                  padding: 2px 20px;
+                }
+                #idle_inhibitor {
+                  color: #${base09};
+                  background: #${base00};
+                  border-radius: 15px 15px 15px 15px;
+                  margin: 2px;
+                  padding: 2px 20px;
+                }
+                #custom-exit {
+                  color: #${base0E};
+                  background: #${base00};
+                  border-radius: 15px 0px 0px 15px;
+                  margin: 2px 0px 2px 2px;
+                  padding: 2px 20px;
+                }
         ''
       ];
     };
