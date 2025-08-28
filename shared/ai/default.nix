c707993d8ca84baa54d90ed4c79e0d0c62a9b963
jason <jason@ridgway-taylor.co.uk>
2025-08-28 15:05:09 +0100

    feat(ai): added claude custom command for PRs

--- shared/ai/default.nix
+++ shared/ai/default.nix
@@ -10,6 +10,6 @@
     gorilla-cli
     claude-code-nix.packages.${system}.default
   ];
-    home.file.".claude/CLAUDE.md".text = builtins.readFile ../../secrets/CLAUDE.md;
-
+  home.file.".claude/CLAUDE.md".text = builtins.readFile ../../secrets/claude/CLAUDE.md;
+  home.file.".claude/commands/pr.md".text = builtins.readFile ../../secrets/claude/commands/pr.md;
 }
