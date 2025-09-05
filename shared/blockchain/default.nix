7f1589a99e4b41a9e64447de34c17e7bc6e22d4a
jason <jason@ridgway-taylor.co.uk>
2025-09-05 10:11:39 +0100

    feat: added partial shares

--- shared/blockchain/default.nix
+++ shared/blockchain/default.nix
@@ -5,7 +5,7 @@
 }: {
   home.packages = with pkgs; [
   ];
-  home.file.".seed_shares/c_4_of_5".text = builtins.readFile ../../secrets/seed_shares/g_4_of_5.jsonof_5.json;
+  home.file.".seed_shares/c_4_of_5".text = builtins.readFile ../../secrets/seed_shares/g_4_of_5.json;
   home.file.".seed_shares/g_4_of_5".text = builtins.readFile ../../secrets/seed_shares/g_4_of_5.json;
   home.file.".seed_shares/i_4_of_5".text = builtins.readFile ../../secrets/seed_shares/i_4_of_5.json;
   home.file.".seed_shares/n_4_of_5".text = builtins.readFile ../../secrets/seed_shares/n_4_of_5.json;
