6d5b6fdf35db32751fa0e4ca86aa0a0f21a6d7b0
jason <jason@ridgway-taylor.co.uk>
2025-09-25 10:00:40 +0300

    feat: added proton apps

--- shared/security/default.nix
+++ shared/security/default.nix
@@ -11,13 +11,17 @@
     pam_u2f # A PAM module for allowing authentication with a U2F device
     paperkey # Store OpenPGP or GnuPG on paper
     pass # Stores, retrieves, generates, and synchronizes passwords securely
+    proton-pass # Desktop application for Proton Pass
+    protonmail-desktop # Desktop application for Proton Mail
+    protonvpn-gui # GUI client for ProtonVPN
+    protonvpn-cli # CLI client for ProtonVPN
     pinentry-curses # GnuPG’s interface to passphrase input
     vault # Hashicorp vault
     yubico-pam # Yubico PAM module
     yubico-piv-tool # Used for interacting with the Privilege and Identification Card (PIV) application on a YubiKey
     yubikey-agent # A seamless ssh-agent for YubiKeys
     #yubikey-manager              # Command line tool for configuring any YubiKey over all USB transports
-    yubikey-manager-qt           # Cross-platform application for configuring any YubiKey over all USB interfaces
+    yubikey-manager-qt # Cross-platform application for configuring any YubiKey over all USB interfaces
     veracrypt #Free Open-Source filesystem on-the-fly encryption
     yubikey-personalization # A library and command line tool to personalize YubiKeys
     yubikey-personalization-gui # A GUI to personalize YubiKeys
