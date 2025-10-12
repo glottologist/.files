a3e497c7811591394187efd775e4dc5541da9bec
jason <jason@ridgway-taylor.co.uk>
2025-10-12 16:09:58 +0300

    fix: stop wifi powersaving

--- hosts/bebop/networking.nix
+++ hosts/bebop/networking.nix
@@ -8,7 +8,10 @@
   environment.systemPackages = with pkgs; [networkmanagerapplet];
   networking = {
     hostName = "bebop"; # Define your hostname.
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      wifi.powersave = false; # Prevent periodic wifi disconnections
+    };
     useDHCP = lib.mkDefault true;
     extraHosts = builtins.readFile ../../secrets/hosts;
   };
