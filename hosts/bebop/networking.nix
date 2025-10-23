f7466d8d85b3f9de7d10d7f3f2afd5d37846dd90
jason <jason@ridgway-taylor.co.uk>
2025-10-23 11:24:20 +0300

    feat: use quad9 for enc dns

--- hosts/bebop/networking.nix
+++ hosts/bebop/networking.nix
@@ -11,9 +11,22 @@
     networkmanager = {
       enable = true;
       wifi.powersave = false; # Prevent periodic wifi disconnections
+      dns = "systemd-resolved";
     };
     useDHCP = lib.mkDefault true;
     extraHosts = builtins.readFile ../../secrets/hosts;
   };
 
+  # Enable systemd-resolved with DNS-over-TLS for Quad9
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "9.9.9.9" "149.112.112.112" ];
+    extraConfig = ''
+      DNSOverTLS=yes
+      DNS=9.9.9.9#dns.quad9.net 149.112.112.112#dns.quad9.net
+      [2620:fe::fe]#dns.quad9.net [2620:fe::9]#dns.quad9.net
+    '';
+  };
 }
