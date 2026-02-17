9a0b0e833f84b12e597d1e250f4a7a2568090fee
jason <jason@ridgway-taylor.co.uk>
2026-02-17 21:56:25 +0000

    feat: added rawtherepee

--- shared/media/default.nix
+++ shared/media/default.nix
@@ -6,13 +6,13 @@
   home.packages = with pkgs; [
 
       darktable            # Virtual lighttable and darkroom for photographers
+      kazam                # A screencasting program created with design in mind
       obs-studio           # Free and open source software for video recording and live streaming
       plex-desktop         # Streaming media player for Plex
+      rawtherapee          # RAW converter and digital photo processing software
+      simplescreenrecorder # A screen recorder for Linux
       spotify              # Play music from the Spotify music service
       vlc                  # Cross-platform media player and streaming server
-      simplescreenrecorder # A screen recorder for Linux
       vokoscreen-ng        # Simple GUI screencast recorder, using ffmpeg
-      kazam                # A screencasting program created with design in mind
-
   ];
 }
