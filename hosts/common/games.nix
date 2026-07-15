d9a0882b602b96c79f59c4b1b0e2914feb5268a4
jason <jason@ridgway-taylor.co.uk>
2026-07-15 16:54:18 +0100

    feat: localize teach artefacts and enable steam gaming stack

--- hosts/common/games.nix
+++ hosts/common/games.nix
@@ -4,6 +4,16 @@
   ...
 }: {
   programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      gamescopeSession.enable = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
+    };
     gamescope = {
       enable = true;
       capSysNice = true;
@@ -12,7 +22,12 @@
         "--expose-wayland"
       ];
     };
+    gamemode.enable = true;
   };
+
+  hardware.steam-hardware.enable = true;
+
   environment.systemPackages = with pkgs; [
+    steam-run
   ];
 }
